fa1112f5595552ace810c82ac02f62b62a322925
Raphael Cardoso <raphaelpetrere@gmail.com>
2021-07-16 21:47:33 -0300

    Basic structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,32 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  
+  ${AlurakutStyles}
+`
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
